2f96d8aa7972256c2797294d13cf3ddb9ddc96d0
Illensky <alexis.laroche.02240@gmail.com>
2021-12-21 11:52:49 +0100

    Exercice done

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 // FIXME Assurez vous que ces deux variables contiennent bien les bonnes choses ;-)
 let player = $('.player__video').first();
-let progress = $('.progress__filled').first();
+let progress = $('.progress').first();
+const playerbtn = $('.toggle')
+console.log(playerbtn.text())
+playerbtn.click(function () {
+
+    if (playerbtn.text() === "►") {
+        playerbtn.text("⏸")
+        player.get(0).play()
+    } else {
+        playerbtn.text("►")
+        player.get(0).pause()
+    }
+})
+
+const progressBar = document.createElement("div")
+progress[0].appendChild(progressBar)
+progressBar.style.height = "80%"
+progressBar.style.backgroundColor = "white"
+
+player.get(0).addEventListener("timeupdate", function () {
+    progressBar.style.width = ((player.get(0).currentTime / 596) * 100).toString() + "%"
+})
